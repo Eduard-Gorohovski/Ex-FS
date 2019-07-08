e348fbae33dc090b1866ec501f0162cfba85506e
Eduard-Gorohovski <edis.email@gamil.com>
2019-07-08 16:28:39 +0300

    1.8b

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -11,11 +11,10 @@ const Header = (props) => {
 
 const Button = (props) => {
     return(
-        <>
-            <button onClick={props.function}>
-                {props.text}
-            </button>
-        </>
+        <button onClick={props.function}>
+            {props.text}
+        </button>
+
     )
 }
 
@@ -28,29 +27,34 @@ const DisplayAttribute = (props) => {
     )
 }
 
+const Statistics = ({good, neutral, bad}) => {
+    const all=good+neutral+bad
+    return(
+        <>
+            <Header headline='statistics' />
+            <DisplayAttribute attribute="good" result={good} />
+            <DisplayAttribute attribute="neutral" result={neutral} />
+            <DisplayAttribute attribute="bad" result={bad} />
+            <DisplayAttribute attribute="all" result={all} />
+            <DisplayAttribute attribute="average" result={(good-bad)/all} />
+            <DisplayAttribute attribute="positive" result={100*(good)/all + " %"} />
+        </>
+    )
+}
+
 const App = () => {
   // save clicks of each button to own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
   const headline='give feedback'
-  const statistics='statistics'
-  const all=good+neutral+bad;
-
   return (
     <div>
         <Header headline={headline} />
         <Button function={()=>{setGood(good+1)}} text='good'/>
         <Button function={()=>{setNeutral(neutral+1)}} text='neutral'/>
         <Button function={()=>{setBad(bad+1)}} text='bad'/>
-        <Header headline={statistics} />
-        <DisplayAttribute attribute="good" result={good} />
-        <DisplayAttribute attribute="neutral" result={neutral} />
-        <DisplayAttribute attribute="bad" result={bad} />
-        <DisplayAttribute attribute="all" result={all} />
-        <DisplayAttribute attribute="average" result={(good-bad)/all} />
-        <DisplayAttribute attribute="positive" result={100*(good)/all + " %"} />
-        
+        <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
